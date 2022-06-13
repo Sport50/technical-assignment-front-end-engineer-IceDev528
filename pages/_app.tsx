902d8946011085be482d837e0e4a4986a6c55d5e
IceDev528 <iceprince.L528@gmail.com>
2022-06-13 07:18:42 -0500

    Update metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Clubee Articles</title>
+        <title>Clubee Articles Test</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <meta name="description" content="Clubee Article" />
+        <meta name="description" content="Clubee Article Test" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <Layout>
